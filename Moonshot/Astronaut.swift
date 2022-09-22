e9794a9bc9e1a3c98666a8f9adfa69a715b818c2
Carlossiii <cv5673@gmail.com>
2022-09-22 13:38:59 -0300

    Basic layout and main View functionalities done.

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Carlos Vinicius on 22/09/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
